38687dc9da710259fa2a563611901df2ba856a3f
Rahmania Astrid Mochtar <rahmania.astrid@ui.ac.id>
2020-04-01 15:24:02 +0700

    add corsheader package

--- disguise/views.py
+++ disguise/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, FileResponse, HttpResponseRedirect
 from django.urls import reverse
-import zipfile
+from zipfile import ZipFile
 import requests
 
 from .models import Disguise, Document
@@ -25,6 +25,28 @@ class DocumentViewSet(viewsets.ModelViewSet):
 def upload_form_view(request):
     if request.method=="POST":
         uploaded_file = request.FILES['myfile']
+
+        url = 'https://localhost:8001/file/zip'
+        header = {
+            'X-ROUTING-KEY': ''
+        }
+        data = {
+            'file': uploaded_file,
+            'filename': uploaded_file.name
+        }
+        requests.post()
         return render(request, 'progressbar.html', {})
     
     return render(request,'upload_form.html', {})
+
+def zip_worker(request):
+    if request.method=="POST":
+        uploaded_file = request.data.file
+        zip_name = '{}.zip'.format(request.data.filename)
+
+        zip_file = ZipFile(zip_name,'w') 
+        zip_file.write(uploaded_file)
+
+    #     return render(request, 'progressbar.html', {})
+    
+    # return render(request,'upload_form.html', {})
